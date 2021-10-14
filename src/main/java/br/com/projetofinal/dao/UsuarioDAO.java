431f1bc7393160dff9ed1c8476c07db1b2210728
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-10-14 18:58:30 -0300

    ação especifica com CRUD, assinatura do método de login

--- src/main/java/br/com/projetofinal/dao/UsuarioDAO.java
+++ src/main/java/br/com/projetofinal/dao/UsuarioDAO.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.projetofinal.beans.Usuario;
 
 public interface UsuarioDAO extends CrudRepository<Usuario, Integer>{
-
+	public Usuario findByEmailAndSenha(String email, String senha);
 }
