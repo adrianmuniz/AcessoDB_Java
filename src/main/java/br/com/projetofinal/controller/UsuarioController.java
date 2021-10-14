4b42489a0e087507da09c017d7514c590d3935ff
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-10-14 17:12:42 -0300

    Chamada HTTP via URL (Pelo Id)

--- src/main/java/br/com/projetofinal/controller/UsuarioController.java
+++ src/main/java/br/com/projetofinal/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.projetofinal.beans.Usuario;
@@ -20,6 +21,21 @@ public class UsuarioController {
 	public ResponseEntity<List<Usuario>> getAll(){
 		
 		List<Usuario> retorno = (List<Usuario>) dao.findAll();
+		if(retorno.size() == 0) {
+			return ResponseEntity.status(404).build();
+		}
+		
+		return ResponseEntity.ok(retorno);
+	}
+	
+	@GetMapping("/usuario/{id}")
+	public ResponseEntity<Usuario> getUsuario(@PathVariable int id) {
+		
+		Usuario retorno = (Usuario)dao.findById(id).orElse(null);
+		
+		if(retorno == null) {
+			return ResponseEntity.status(404).build();
+		}
 		
 		return ResponseEntity.ok(retorno);
 	}
