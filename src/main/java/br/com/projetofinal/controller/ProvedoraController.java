40bf1eb41577f438f6c52800d56d4b98adc1adfa
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-10-20 21:13:22 -0300

    Get Provedoras

--- src/main/java/br/com/projetofinal/controller/ProvedoraController.java
+++ src/main/java/br/com/projetofinal/controller/ProvedoraController.java
@@ -1,8 +1,11 @@
 package br.com.projetofinal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,14 @@ public class ProvedoraController {
 			e.printStackTrace();
 			return ResponseEntity.status(403).build();
 		}
+	}
+	
+	@GetMapping("/provedoras")
+	public ResponseEntity<List<Provedora>> getAll() {
+		List<Provedora> provedoras = (List<Provedora>) dao.findAll();
+		if(provedoras.size()==0) {
+			return ResponseEntity.status(404).build();
+		}
+		return ResponseEntity.ok(provedoras);
 	}
 }
